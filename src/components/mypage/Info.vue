51d2c7dd46624241c08e0175fa46c2cf6ba6ba62
daersh <daersh@naver.com>
2024-04-22 15:25:19 +0900

    [FE] refactor: 내 정보 화면 수정

--- src/components/mypage/Info.vue
+++ src/components/mypage/Info.vue
@@ -2,57 +2,58 @@
   <div class="popup-container" @click.self="closePopup">
     <div class="popup-content">
       <div class="vector-group">
-        <img
+        <!-- <img
             class="vector-icon3"
             alt=""
-            src="../../assets/icon/MyPage/x.png"
-        />
-        <div class="wrapper">
-          <h1 class="h1">로그인 정보</h1>
-        </div>
+            src="@/assets/icon/MyPage/x.png"
+        /> -->
       </div>
-      <div class="frame-container">
-        <div class="frame-group">
-          <div class="container">
-            <div class="div3">닉네임</div>
-          </div>
-          <div class="frame-div">
-            <div class="konan-parent">
-              <b class="konan1">{{ nickname }}</b>
-              <div class="frame">
-                <div class="div4">변경하기</div>
-              </div>
-            </div>
-          </div>
-          <div class="line-div"></div>
-        </div>
-      </div>
-      <div class="frame-wrapper1">
-        <div class="frame-parent1">
-          <div class="wrapper1">
-            <div class="div5">연결된 계정</div>
-          </div>
-          <div class="frame-parent2">
-            <img
-                class="frame-child1"
-                loading="lazy"
-                alt=""
-                src="../../assets/icon/MyPage/google.png"
-            />
-            <div class="dlwldms0902gmailcom-wrapper">
-              <b class="dlwldms0902gmailcom">{{email}}</b>
-            </div>
-          </div>
-          <div class="wrapper2">
-            <div class="div6">변경하기</div>
-          </div>
-        </div>
+
+      <div class="nn">
+        <h1 class="h1" align="center">로그인 정보</h1>
+        <br>
+        <div style="color:gray; " >닉네임 &nbsp;  </div>
+        <br>
+        <b class="konan1" style="margin-top: 30px ;">{{ nickname }}</b><br>
+        <div class="click-button" type="button"  value="변경하기" >변경하기</div>
+        <br><hr><br>
+        <div style="color:gray"> 연결된 계정</div><br>
+        <img src="@/assets/icon/MyPage/google.png" style="height: 18px;"/> &nbsp;
+        <b class="dlwldms0902gmailcom">{{email}}</b>
+        <br>
+        <div class="click-button" type="button"  value="변경하기" >변경하기</div>
       </div>
     </div>
   </div>
 </template>
 
+<script setup>
+
+
+</script>
+
 <style scoped>
+.click-button {
+  font-size: 12px;
+  vertical-align: middle;
+  float: right;
+  color: gray;
+  cursor: pointer; /* 마우스를 올리면 포인터로 변경 */
+}
+
+.click-button:hover {
+  color: black; /* 마우스 오버 시 색 변화 */
+  font-size: 13px;
+}
+
+.click-button:active {
+  transform: translateY(1px); /* 클릭 시 아래로 이동하는 효과 */
+}
+
+
+.nn{
+  padding: 5%;
+}
 .popup-container {
   position: fixed;
   top: 0;
@@ -69,6 +70,8 @@
 .popup-content {
   background-color: white;
   padding: 20px;
+  width: 30%;
+  height: 35%;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 }
